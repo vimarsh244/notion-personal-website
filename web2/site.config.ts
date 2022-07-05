daf6e1ef6a4f833ea7a3a8b8ef59a939c680426c
Vimarsh <vimarsh244@gmail.com>
2022-07-05 08:35:46 +0000

    new version

--- web2/site.config.ts
+++ web2/site.config.ts
@@ -2,25 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '60e32b8633fc4c65b91e64a42877687f',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '88c4709d-f416-4a44-98c6-fce37a3724d6',
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Vimarsh',
+  domain: 'vimarsh.info',
+  author: 'Vimarsh Shah',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Thoughts of a 17 y/o maker and learner',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
+  twitter: 'vimarshtweets',
+  github: 'vimarsh244',
+  linkedin: 'vimarsh-shah',
+  newsletter: 'https://newsletter.vimarsh.info/', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
@@ -54,11 +55,11 @@ export default siteConfig({
   // navigationLinks: [
   //   {
   //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //     pageId: '5cc4b3bb61104033879a939219feda95'
   //   },
   //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //     title: 'Projects',
+  //     pageId: '66d74e05c0aa47f8b311af231a8cac0c'
   //   }
   // ]
 })
