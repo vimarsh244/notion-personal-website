cbd1c814ce4892a16a90b1f335c88946a139a4db
Vimarsh <vimarsh244@gmail.com>
2022-09-24 10:39:46 +0000

    again enablid lqip preview

--- web2/next.config.js
+++ web2/next.config.js
@@ -14,7 +14,8 @@ module.exports = withBundleAnalyzer({
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com'
     ],
-    formats: ['image/avif', 'image/webp'],
+    // formats: ['image/avif', 'image/webp'],
+    formats: ['image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   }
